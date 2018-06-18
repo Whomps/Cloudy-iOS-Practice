f6e67967f620c1080425f54e371d06337af7dc26
Derek <derek@HEBs-iMac.local>
2018-06-18 15:13:12 -0500

    Checkpoint - Starting: Taking MVVM to the Next Level

--- CloudyTests/Test Cases/WeatherDayViewViewModelTests.swift
+++ CloudyTests/Test Cases/WeatherDayViewViewModelTests.swift
@@ -1,5 +1,5 @@
 //
-//  DayViewViewModelTests.swift
+//  WeatherDayViewModelTests.swift
 //  CloudyTests
 //
 //  Created by Derek on 6/18/18.
@@ -10,11 +10,11 @@ import XCTest
 
 @testable import Cloudy
 
-class DayViewViewModelTests: XCTestCase {
+class WeatherDayViewViewModelTests: XCTestCase {
     
     // MARK: - Properties
     
-    var viewModel: DayViewViewModel!
+    var viewModel: WeatherDayViewViewModel!
     
     // MARK: - Set Up & Tear Down
     
@@ -25,61 +25,42 @@ class DayViewViewModelTests: XCTestCase {
         let data = loadStubFromBundle(withName: "darksky", extension: "json")
         let weatherData: WeatherData = try! JSONDecoder.decode(data: data)
         
-        // Intitialize View Model
-        viewModel = DayViewViewModel(weatherData: weatherData)
+        // Initialize View Model
+        viewModel = WeatherDayViewViewModel(weatherDayData: weatherData.dailyData[5])
     }
     
     override func tearDown() {
         super.tearDown()
         
-        // Reset User Defaults
-        UserDefaults.standard.removeObject(forKey: UserDefaultsKeys.timeNotation)
-        UserDefaults.standard.removeObject(forKey: UserDefaultsKeys.temperatureNotation)
+        //Reset User Defaults
         UserDefaults.standard.removeObject(forKey: UserDefaultsKeys.unitsNotation)
+        UserDefaults.standard.removeObject(forKey: UserDefaultsKeys.temperatureNotation)
+    }
+    
+    // MARK: - Tests for Day
+    
+    func testDay() {
+        XCTAssertEqual(viewModel.day, "Saturday")
     }
     
     // MARK: - Tests for Date
-    
     func testDate() {
-        XCTAssertEqual(viewModel.date, "Tue, July 11")
-    }
-    
-    // MARK: - Tests for Time
-    
-    func testTime_TwelveHour() {
-        let timeNotation: TimeNotation = .twelveHour
-        UserDefaults.standard.set(timeNotation.rawValue, forKey: UserDefaultsKeys.timeNotation)
-        
-        XCTAssertEqual(viewModel.time, "06:57 AM")
-    }
-    
-    func testTime_TwentyFourHour() {
-        let timeNotation: TimeNotation = .twentyFourHour
-        UserDefaults.standard.set(timeNotation.rawValue, forKey: UserDefaultsKeys.timeNotation)
-        
-        XCTAssertEqual(viewModel.time, "06:57")
-    }
-    
-    // MARK: - Tests for Summary
-    
-    func testSummary() {
-        XCTAssertEqual(viewModel.summary, "Clear")
+        XCTAssertEqual(viewModel.date, "July 15")
     }
     
     // MARK: - Tests for Temperature
-    
     func testTemperature_Fahrenheit() {
         let temperatureNotation: TemperatureNotation = .fahrenheit
         UserDefaults.standard.set(temperatureNotation.rawValue, forKey: UserDefaultsKeys.temperatureNotation)
         
-        XCTAssertEqual(viewModel.temperature, "44.5 °F")
+        XCTAssertEqual(viewModel.temperature, "37 °F - 47 °F")
     }
     
     func testTemperature_Celsius() {
         let temperatureNotation: TemperatureNotation = .celsius
         UserDefaults.standard.set(temperatureNotation.rawValue, forKey: UserDefaultsKeys.temperatureNotation)
         
-        XCTAssertEqual(viewModel.temperature, "6.9 °C")
+        XCTAssertEqual(viewModel.temperature, "3 °C - 8 °C")
     }
     
     // MARK: - Tests for Wind Speed
@@ -88,14 +69,14 @@ class DayViewViewModelTests: XCTestCase {
         let unitsNotation: UnitsNotation = .imperial
         UserDefaults.standard.set(unitsNotation.rawValue, forKey: UserDefaultsKeys.unitsNotation)
         
-        XCTAssertEqual(viewModel.windSpeed, "6 MPH")
+        XCTAssertEqual(viewModel.windSpeed, "1 MPH")
     }
     
     func testWindSpeed_Metric() {
         let unitsNotation: UnitsNotation = .metric
         UserDefaults.standard.set(unitsNotation.rawValue, forKey: UserDefaultsKeys.unitsNotation)
         
-        XCTAssertEqual(viewModel.windSpeed, "10 KPH")
+        XCTAssertEqual(viewModel.windSpeed, "2 KPH")
     }
     
     // MARK: - Tests for Image
@@ -103,13 +84,12 @@ class DayViewViewModelTests: XCTestCase {
     func testImage() {
         let viewModelImage = viewModel.image
         let imageDataViewModel = UIImagePNGRepresentation(viewModelImage!)!
-        let imageDataReference = UIImagePNGRepresentation(UIImage(named: "clear-day")!)!
+        let imageDataReference = UIImagePNGRepresentation(UIImage(named: "cloudy")!)!
         
         XCTAssertNotNil(viewModelImage)
         XCTAssertEqual(viewModelImage!.size.width, 236.0)
-        XCTAssertEqual(viewModelImage!.size.height, 236.0)
+        XCTAssertEqual(viewModelImage!.size.height, 172.0)
         XCTAssertEqual(imageDataViewModel, imageDataReference)
-        
     }
     
 }
