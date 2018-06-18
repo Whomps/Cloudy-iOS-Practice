ff29a420ef88a9a0402b85c49a432f65ed0bfb12
Derek <derek@HEBs-iMac.local>
2018-06-18 10:51:17 -0500

    Checkpoint - Starting: Supercharing MVVM With Protocols

--- Cloudy/View Controllers/SettingsViewController/SettingsViewController.swift
+++ Cloudy/View Controllers/SettingsViewController/SettingsViewController.swift
@@ -114,8 +114,7 @@ extension SettingsViewController: UITableViewDataSource, UITableViewDelegate {
         
         if let viewModel = viewModel {
             // Configure Cell
-            cell.mainLabel.text = viewModel.text
-            cell.accessoryType = viewModel.accessoryType
+            cell.configure(withViewModel: viewModel)
         }
         
         return cell
